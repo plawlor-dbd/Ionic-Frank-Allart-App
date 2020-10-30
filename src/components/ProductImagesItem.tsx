828d853a84df69ff2f3b63cd81ba911ee905f9cc
Peter Lawlor <pete@wearedbd.co.uk>
2020-10-30 12:48:25 +0000

    Adding Prev/Next Buttons

--- src/components/ProductImagesItem.tsx
+++ src/components/ProductImagesItem.tsx
@@ -21,34 +21,80 @@ interface ProductImagesItemProps {
 
 const ProductImagesItem: React.FC<ProductImagesItemProps> = ({ productImages }) => {
   const [showModal, setShowModal] = useState(false);
+  const [hasPrevProductImage, setHasPrevProductImage] = useState(false);
+  const [hasNextProductImage, setHasNextProductImage] = useState(false);
+
+  const prevNextStyle = {
+      fontSize: "10px",
+  }
 
   return (
-        <IonItem  id="prodImgItem">
+
+    
+        <IonItem id="prodImgItem">          
           <IonThumbnail id="prodImage" slot="start">
-            <IonImg class="productImgBtn" onClick={() => setShowModal(true)} src={productImages.src} />
-            <IonModal  isOpen={showModal} cssClass='productModal'>
+            <IonImg class="productImgBtn" id={"modal-" + productImages.id.toString()} onClick={() => setShowModal(true)} src={productImages.src} />
+            <IonModal  isOpen={showModal} cssClass='productModal' onWillPresent={function(){
+                const prevImg = document.getElementById("modal-" + (productImages.id-1)); 
+                const nextImg = document.getElementById("modal-" + (productImages.id+1));
+                console.log(productImages.id+1); 
+                console.log(prevImg);
+                console.log(nextImg);
+                if(!prevImg){setHasPrevProductImage(false);}else{setHasPrevProductImage(true);}
+                if(!nextImg){  setHasNextProductImage(false);}else{setHasNextProductImage(true);}}}>
               <IonHeader translucent>
             <IonToolbar>
               <IonButtons slot="end">
-                <IonButton onClick={() => setShowModal(false)}>Close</IonButton>
+                <IonButton  color="dark" id={"close-btn-" + productImages.id.toString()} onClick={() => setShowModal(false)}>Close</IonButton>
               </IonButtons>
             </IonToolbar>
           </IonHeader>
           <IonContent>
           <TransformWrapper>
           <TransformComponent>
-            <img src={productImages.src} alt="productImage" />
+          <img src={productImages.src} alt="productImage" />
           </TransformComponent>
           </TransformWrapper>
-            <p id="prodTitle">
+          <div style={{display: "inline-block", margin: "0 auto" }}>
+            {hasPrevProductImage &&
+              <IonButton size="small" style={prevNextStyle} color="dark" className="prev-next-btn" id={"prev-btn-" + productImages.id.toString()} onClick={() => previousSlide((productImages.id).toString())}>&lt;</IonButton>
+            }
+            
+            {hasNextProductImage &&
+              <IonButton size="small" style={prevNextStyle} color="dark"  className="prev-next-btn" id={"next-btn-" + productImages.id.toString()} onClick={() => nextSlide((productImages.id).toString())}>&gt;</IonButton>
+            }
+            </div>
+            <p style={{marginTop:"1px"}} id="prodTitle">
               {productImages.title}
             </p>
           </IonContent>
           </IonModal>
         </IonThumbnail>
       </IonItem>
+
+      
   );
 };
 
 
+
+
+function previousSlide(id){
+  console.log(id);
+  document.getElementById("close-btn-" + id.toString()).click();
+  id--;
+  document.getElementById("modal-" + (id).toString()).click(); 
+}
+
+function nextSlide(id){
+  console.log(id);
+  document.getElementById("close-btn-" + id.toString()).click();
+  id++;
+  document.getElementById("modal-" + (id).toString()).click();
+}
+
+
+
+
 export default ProductImagesItem;
+
